73280c91ca5771948466ffc1b317964e8ef2a6c6
kutay <yamankutay1@gmail.com>
2020-08-22 03:27:39 -0700

    Initial Commit

---
+++ CatchItGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchItGameApp
+//
+//  Created by kutayyaman on 22.08.2020.
+//  Copyright © 2020 kutayyaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
